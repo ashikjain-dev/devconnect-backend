da1815df45e338ab03345f9117ff4d083ba643d1
ashik-jain <ashikjain33@gmail.com>
2025-07-31 21:15:46 +0530

    feature:added route signup to save a user in mongo database

--- src/index.js
+++ src/index.js
@@ -3,7 +3,25 @@ const express = require("express");
 const app = express();
 
 const { mongoConnect } = require("./config/mongo");
+const { User } = require("./models/user");
 
+//to read data of json and urlencoded format
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//sign up a user
+app.post("/signup", async (req, res) => {
+  try {
+    console.log("req body", req.body);
+    const userObj = req.body;
+    const user = new User(userObj);
+    await user.save();
+    res.send("User data saved successfully.");
+  } catch (error) {
+    console.log("error while saving a user data".error);
+    res.status(401).send("Not able to save data.");
+  }
+});
 mongoConnect()
   .then(() => {
     console.log("connection is successful.");

305c76ad9b03b9c9c71e35834720a640d4dbc32f
ashik-jain <ashikjain33@gmail.com>
2025-08-02 15:31:38 +0530

    fix:store connection string in env file and use dotenv to load values

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
-const URI =
-  "mongodb+srv://ashikjain44:IdrACDel59VyJFBr@cluster0.g99wkhh.mongodb.net/DevConnect";
+require("dotenv").config();
+
+const URI = process.env.DB_CONNECTION_STRING;
 const mongoConnect = async () => {
   await mongoose.connect(URI);
 };
